34db4b198168a20bd7470cd795f07212a9bda734
igorok1 <43265475+igorok1@users.noreply.github.com>
2025-06-04 11:59:40 +0300

    Update check_parity.py

--- check_parity.py
+++ check_parity.py
@@ -7,14 +7,14 @@ def check_parity(n: int) -> str:
     Повертає:
         str: "Парне" якщо n парне, і "Непарне" інакше
     """
-    return "Парне" if n % 2 == 0 else "Непарне"
+    return "Even" if n % 2 == 0 else "Odd"
 
 
 if __name__ == "__main__":
     try:
-        number = int(input("Введіть число: "))
+        number = int(input("Enter number: "))
     except ValueError:
-        print("Помилка: введено не число")
+        print("Error: it is not number")
     else:
         print(check_parity(number))
 
